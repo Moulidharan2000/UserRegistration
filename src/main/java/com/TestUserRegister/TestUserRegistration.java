e2629844da6e56bb007ff4f2c6e131fd2f4b5401
Moulidharan <116242759+Moulidharan2000@users.noreply.github.com>
2022-12-07 14:21:38 +0530

    added : Junit Test Case for Validating UserRegistration

--- src/main/java/com/TestUserRegister/TestUserRegistration.java
+++ src/main/java/com/TestUserRegister/TestUserRegistration.java
@@ -12,14 +12,14 @@ public class TestUserRegistration {
 	    @Test
 	    public void givenCorrectDetailsReturnHappy() {
 			UserRegistration validate = new UserRegistration();
-			String result = validate.userRegistrationValidation("Tekesh", "Singh", "tekesh.singh@gmail.co.in", "91 1234567890", "Tekesh1Singh@");
+			String result = validate.userRegistrationValidation("Daniel", "Mike", "dani.mikeg@gmail.co.in", "91 1234567890", "mikedaniel@");
 			Assert.assertEquals("VALID", result);
 		}
 
 	    @Test
 	    public void givenIncorrectDetailsReturnSad() {
 			UserRegistration validate = new UserRegistration();
-			String result = validate.userRegistrationValidation("tekesh", "singh", "tekesh-Singh@gmail.co.in1", "919087654321", "Tekesh@");
+			String result = validate.userRegistrationValidation("daniel", "mike", "mike-daniel@gmail.co.in1", "919856321456", "DMike@");
 			Assert.assertEquals("INVALID", result);
 		}
 	}
